6b9aa387c99e4a4012329d1956dc9f7427667fb8
ivan-pl <pleskaiv@gmail.com>
2022-12-12 15:20:42 +0300

    build: setup RTL

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
+
+import App from "./App";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLDivElement
 );
-root.render(<h1>Hello, world!</h1>);
+root.render(<App />);
--- src/sum.test.ts
+++
@@ -1,5 +0,0 @@
-import sum from "./sum";
-
-it("sum()", () => {
-  expect(sum(2, 3)).toBe(5);
-});
